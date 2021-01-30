4d6af3ccd3c0dd1c85c6d727fa664cfb8eb79dcf
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2021-01-30 15:41:08 -0500

    added one, two files

--- one_RPI_mine.py
+++ one_RPI_mine.py
@@ -3,9 +3,9 @@ import paramiko
 RPI_1 = paramiko.SSHClient()
 
 RPI_1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-RPI_1.connect(hostname='localhost',username='pi',port=5022)
+RPI_1.connect(hostname='localhost',username='pi',port=5022,password="raspberry")
     
-stdout=RPI_1.exec_command('python3 testMine.py')
+stdin,stdout,stder=RPI_1.exec_command('python3 testMine.py 5')
 output = stdout.readlines()
 for items in output:
     print("RPI_1:" + items)
