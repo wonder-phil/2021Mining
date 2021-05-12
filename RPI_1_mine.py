714a733fbacac8862aed5ea576092d99d5bbb0a9
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2021-05-12 17:02:39 -0400

    New

---
+++ RPI_1_mine.py
@@ -0,0 +1,18 @@
+import paramiko
+
+class RPI_1_Mine():
+    def __init__(self,difficulty):
+        RPI_1 = paramiko.SSHClient()
+
+        RPI_1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        RPI_1.connect(hostname='localhost',username='pi',port=5022,password="raspberry")
+        stdin,stdout,stder=RPI_1.exec_command('python3 testMine.py ' + str(difficulty))
+        output = stdout.readlines()
+        for items in output:
+            print("RPI_1:" + items)
+
+        RPI_1.close()
+
+
+rpi_1 = RPI_1_Mine(3)        
+        
